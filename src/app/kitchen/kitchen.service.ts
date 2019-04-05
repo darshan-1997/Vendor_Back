9518005fa177b93cf03768733cb31554b6a2966d
darshan-1997 <darshannayak712@gmail.com>
2019-04-06 00:51:55 +0530

    Update kitchen.service.ts

--- src/app/kitchen/kitchen.service.ts
+++ src/app/kitchen/kitchen.service.ts
@@ -14,19 +14,19 @@ constructor(private http:HttpClient) { }
 getConfOrders()
 {
 //console.log("called 213!");
-return this.http.get('https://git.heroku.com/shrouded-refuge-34273.git').pipe(map(res=> res));
+return this.http.get('https://git.heroku.com/shrouded-refuge-34273.git/kitchen/confirmedorders').pipe(map(res=> res));
 }
 
 cancelOrd(id)
 {
     //console.log(id);
-    return this.http.delete('https://git.heroku.com/shrouded-refuge-34273.git'+id).pipe(map(res=>res));
+    return this.http.delete('https://git.heroku.com/shrouded-refuge-34273.git/kitchen/cancelOrder/'+id).pipe(map(res=>res));
 }
 
 
 kitchenorder()
 {
-    return this.http.get('https://git.heroku.com/shrouded-refuge-34273.git').pipe(map(res=> res));
+    return this.http.get('https://git.heroku.com/shrouded-refuge-34273.git/kitchen/kitchenorders').pipe(map(res=> res));
 }
 
 }
