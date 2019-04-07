ed4f374f6d63cad345a78edc5cb39058ba60b281
darshan-1997 <darshannayak712@gmail.com>
2019-04-07 23:00:25 +0530

    Update booker.service.ts

--- src/app/Booker/booker.service.ts
+++ src/app/Booker/booker.service.ts
@@ -19,46 +19,46 @@ const fd= new FormData();
 var headers =new HttpHeaders();
 fd.append('itemImage',itemImage,itemImage.name);
 console.log("Image has been Uploaded");
-return this.http.post('http://localhost:3000/services/addImage',fd,{headers:headers}).pipe(map(res=>res));
+return this.http.post('https://git.heroku.com/shrouded-refuge-34273.git/services/addImage',fd,{headers:headers}).pipe(map(res=>res));
 }
 
 addService(newService){
     var headers =new HttpHeaders();
     headers.append('Content-Type','application/json');
-    return this.http.post('http://localhost:3000/services/addservice',newService,{headers:headers}).pipe(map(res=>res));
+    return this.http.post('https://git.heroku.com/shrouded-refuge-34273.git/services/addservice',newService,{headers:headers}).pipe(map(res=>res));
 
 }
 
 getMenu()
 {
-return this.http.get('http://localhost:3000/menucard/allmenu').pipe(map(res=> res));
+return this.http.get('https://git.heroku.com/shrouded-refuge-34273.git/menucard/allmenu').pipe(map(res=> res));
 }
 
 getReq()
 {
 //console.log("called 213!");
-return this.http.get('http://localhost:3000/services/allreq').pipe(map(res=> res));
+return this.http.get('https://git.heroku.com/shrouded-refuge-34273.git/services/allreq').pipe(map(res=> res));
 }
 
 
 getFeat()
 {
 console.log("called getfeat!");
-return this.http.get('http://localhost:3000/services/getfeat').pipe(map(res=> res));
+return this.http.get('https://git.heroku.com/shrouded-refuge-34273.git/services/getfeat').pipe(map(res=> res));
 }
 
 
 deleteService(id)
 {
     console.log(id);
-    return this.http.delete('http://localhost:3000/services/deleteService/'+id).pipe(map(res=>res));
+    return this.http.delete('https://git.heroku.com/shrouded-refuge-34273.git/services/deleteService/'+id).pipe(map(res=>res));
 }
 
 update(data){
     
     var headers =new HttpHeaders();
     headers.append('Content-Type','application/json');
-    return this.http.post('http://localhost:3000/services/updateservice',data,{headers:headers}).pipe(map(res=>res));
+    return this.http.post('https://git.heroku.com/shrouded-refuge-34273.git/services/updateservice',data,{headers:headers}).pipe(map(res=>res));
 
 }
 updatefeat(data){
@@ -67,7 +67,7 @@ updatefeat(data){
 
     var headers =new HttpHeaders();
     headers.append('Content-Type','application/json');
-    return this.http.post('http://localhost:3000/services/deleteFeat',data,{headers:headers}).pipe(map(res=>res));
+    return this.http.post('https://git.heroku.com/shrouded-refuge-34273.git/services/deleteFeat',data,{headers:headers}).pipe(map(res=>res));
 
 }
 
