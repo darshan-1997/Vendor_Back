0871de4e83ddff4a0585b7eb01b357319fdb2f8f
darshan-1997 <darshannayak712@gmail.com>
2019-04-07 23:04:01 +0530

    Update orders.service.ts

--- src/app/orders/orders.service.ts
+++ src/app/orders/orders.service.ts
@@ -15,7 +15,7 @@ constructor(private http:HttpClient) { }
 getOrders()
 {
 //console.log("called 213!");
-return this.http.get('http://localhost:3000/orders/pendingorders').pipe(map(res=> res));
+return this.http.get('https://git.heroku.com/shrouded-refuge-34273.git/orders/pendingorders').pipe(map(res=> res));
 }
 
 
@@ -23,14 +23,14 @@ return this.http.get('http://localhost:3000/orders/pendingorders').pipe(map(res=
 confirmOrder(order)
 {
 console.log(order);
-return this.http.post('http://localhost:3000/orders/confirmorder',order).pipe(map(res=> res));
+return this.http.post('https://git.heroku.com/shrouded-refuge-34273.git/orders/confirmorder',order).pipe(map(res=> res));
 }
 
 
 GenBill(order)
 {
 console.log(order);
-return this.http.post('http://localhost:3000/orders/GenBill',order).pipe(map(res=> res));
+return this.http.post('https://git.heroku.com/shrouded-refuge-34273.git/orders/GenBill',order).pipe(map(res=> res));
 
 }
 
@@ -39,7 +39,7 @@ return this.http.post('http://localhost:3000/orders/GenBill',order).pipe(map(res
 cancelOrd(id)
 {
     //console.log(id);
-    return this.http.delete('http://localhost:3000/orders/cancelVendor/'+id).pipe(map(res=>res));
+    return this.http.delete('https://git.heroku.com/shrouded-refuge-34273.git/orders/cancelVendor/'+id).pipe(map(res=>res));
 }
 
 
